dd1b4f7470b130a8fdc3b316c1bb30291ad3b695
Pelangi Masita <121101474+angiebow@users.noreply.github.com>
2023-12-30 23:09:11 +0700

    Update intro.tsx

--- portfolio-website-master/components/intro.tsx
+++ portfolio-website-master/components/intro.tsx
@@ -62,11 +62,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Angie.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Angie.</span> a{" "}
+        <span className="font-bold">2nd</span> year{" "}
+        <span className="font-bold">informatics engineering student.</span> I'm really
+        into <span className="italic">artificial intelligence</span>. and{" "}
+        <span className="underline">astrophysics</span>.
       </motion.h1>
 
       <motion.div
