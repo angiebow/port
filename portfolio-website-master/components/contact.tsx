30dc24e91b93bd903dc72b74dfe483b6a883660a
Pelangi Masita <121101474+angiebow@users.noreply.github.com>
2024-01-09 21:36:00 +0700

    Update contact.tsx

--- portfolio-website-master/components/contact.tsx
+++ portfolio-website-master/components/contact.tsx
@@ -33,13 +33,13 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
+        <a className="underline" href="mailto:pelangimasita18@gmail.com">
           pelangimasita18@gmail.com
         </a>{" "}
-        or through this form.
+{/*         or through this form. */}
       </p>
 
-      <form
+{/*       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
@@ -68,7 +68,7 @@ export default function Contact() {
           maxLength={5000}
         />
         <SubmitBtn />
-      </form>
+      </form> */}
     </motion.section>
   );
 }
