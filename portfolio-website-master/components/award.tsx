2e69f2e0b53150367677f10298a50ca866d1e20d
Pelangi Masita <121101474+angiebow@users.noreply.github.com>
2024-01-15 15:13:17 +0700

    Update award.tsx

--- portfolio-website-master/components/award.tsx
+++ portfolio-website-master/components/award.tsx
@@ -7,7 +7,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { experiencesData } from "@/lib/data";
+import { awardData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { useTheme } from "@/context/theme-context";
 
@@ -19,7 +19,7 @@ export default function Award() {
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>Awards</SectionHeading>
       <VerticalTimeline lineColor="">
-        {experiencesData.map((item, index) => (
+        {awardData.map((item, index) => (
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
